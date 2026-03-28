f450c6891d513d288eade631f8042ae0513f08a7
Melvin John <pjmelvin13@gmail.com>
2026-03-28 22:46:49 +0100

    Corrected multiple memory error assignments

--- rtl/vproc_lsu_extension.sv
+++ rtl/vproc_lsu_extension.sv
@@ -270,9 +270,9 @@ module vproc_lsu_extension import vproc_pkg::*; #(
                     rdata_buf_q   <= rdata_buf_d;
                     rdata_off_q   <= rdata_off_d;
                     rmask_buf_q   <= rmask_buf_d;
-                    mem_err_q     <= mem_err_d;
-                    mem_any_err_q <= mem_any_err_d;
-                    mem_exccode_q <= mem_exccode_d;
+                    //mem_err_q     <= mem_err_d;
+                    //mem_any_err_q <= mem_any_err_d;
+                    //mem_exccode_q <= mem_exccode_d;
                 end
             end
         end else begin
@@ -283,6 +283,11 @@ module vproc_lsu_extension import vproc_pkg::*; #(
                 rdata_off_q         = rdata_off_d;
                 rmask_buf_q         = rmask_buf_d;
             end
+            /*always_ff @(posedge clk_i) begin
+                mem_err_q     <= mem_err_d;
+                mem_any_err_q <= mem_any_err_d;
+                mem_exccode_q <= mem_exccode_d;
+            end*/
         end
     endgenerate
 
