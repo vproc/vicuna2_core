34e07c366c578cb5bfe4effc5f8463f4d6aad894
jjones <jefferson.jones@tuwien.ac.at>
2026-08-21 13:59:39 +0200

    increase total number of units to account for new index unit

--- rtl/vproc_pkg.sv
+++ rtl/vproc_pkg.sv
@@ -117,7 +117,7 @@ typedef enum logic [3:0] {
 } op_unit;
 
 // The number of different types of execution units (excludes pseudo-units)
-parameter int unsigned UNIT_CNT = 10;
+parameter int unsigned UNIT_CNT = 11;
 
 typedef enum logic [1:0] {
     COUNT_INC_1 = 2'b00,
