00d338a7bdac28a662be49146ac1bb567e8406e9
Muhammed Efe Cetin <efectn@protonmail.com>
2026-09-14 15:08:50 +0200

    rtl/vproc_unit_wrapper: add pipe_in_mask_valid_i condition to LSU unit valid input signal

--- rtl/vproc_unit_wrapper.sv
+++ rtl/vproc_unit_wrapper.sv
@@ -97,7 +97,7 @@ module vproc_unit_wrapper
       end
 
       logic unit_in_valid_i;
-      assign unit_in_valid_i = &(~(pipe_in_valid_i ^ necessary_ops)) & (|pipe_in_valid_i | pipe_in_mask_valid_i) & ! indexed_op_clear_ready; //Input valid only if all necessary ops are valid (including mask) and not clearing an index operand
+      assign unit_in_valid_i = &(~(pipe_in_valid_i ^ necessary_ops)) & (!pipe_in_ctrl_i.decode_metadata.masked | pipe_in_mask_valid_i) & (|pipe_in_valid_i | pipe_in_mask_valid_i) & ! indexed_op_clear_ready; //Input valid only if all necessary ops are valid (including mask) and not clearing an index operand
 
       logic unit_ready_in_o;
       for (genvar i = 0; i < 2; i++) begin
